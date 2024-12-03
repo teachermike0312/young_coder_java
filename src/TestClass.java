dc5cd2c3cdc91a590afefe9acaaa5ebe749b7b14
ПристрійUNICEF <school12mike@gmail.com>
2024-12-03 16:50:30 +0200

    init commit

---
+++ src/TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass {
+
+   int speed;
+   int weight;
+   String color;
+
+    public TestClass(int speed, int weight, String color) {
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    int testInt = 100;
+}
